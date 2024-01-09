1a3131efee8338063c84b7ef80a835b8e92c93cd
Sergey Shkaev <shkaev.ss@mail.ru>
2024-01-09 20:02:41 +0300

    created class Program - main

---
+++ src/main/java/Program.java
@@ -0,0 +1,11 @@
+import model.Person;
+
+public class Program {
+
+  public static void main(String[] args) {
+
+    Person person1 = new Person("Шкаев", "Сергей", 35);
+    System.out.println(person1);
+
+  }
+}
